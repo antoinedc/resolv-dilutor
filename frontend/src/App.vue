e8628c7c47fe1a3ea1ad8e02c51fb1ce09f6dbce
Antoine de Chevigné <antoine@tryethernal.com>
2025-03-25 16:10:30 +0100

    docs: update dilution rate tooltip text

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -64,13 +64,13 @@
                   <div class="space-y-4">
                     <div>
                       <h4 class="text-sm font-medium text-gray-500 mb-1 flex items-center justify-center">
-                        Share Change
+                        Your Dilution Rate
                         <div class="relative inline-block">
                           <svg @mouseenter="showTooltip = true" @mouseleave="showTooltip = false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 ml-1 text-gray-400 cursor-help">
                             <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
                           </svg>
                           <div v-if="showTooltip" class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm w-64 text-center z-10">
-                            Shows how your share of total points has changed since yesterday. A negative value means your share is increasing (you're earning faster than average), while a positive value means your share is decreasing (you're earning slower than average).
+                            Your dilution rate shows how quickly your share of total points is changing. A negative rate means you're gaining share (earning faster than average), while a positive rate means you're losing share (earning slower than average).
                             <div class="tooltip-arrow absolute top-full left-1/2 transform -translate-x-1/2 -mt-px border-4 border-transparent border-t-gray-900"></div>
                           </div>
                         </div>
